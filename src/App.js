389528c483224f510413a1986b18e819c4bb0018
Bladimir <oirbl@hotmail.com>
2020-02-26 00:34:33 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
 import './App.css';
 
-function App() {
+const DATES = [
+  1582707600,
+  1582718400,
+  1582729200,
+  1582740000,
+  1582750800,
+  1582761600
+];
+
+
+const App = () => {
+
+  const dates = [];
+  DATES.forEach(d => {
+    const date = new Date(d * 1000);
+    const utcString = date.toUTCString();
+    const time = utcString.slice(-11, -4);
+    console.log(`utcString: ${utcString}`);
+    dates.push(time);
+  });
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Suspense className="App" fallback='Loading...!' >
+      <ul>
+        {dates.map(date => {
+          return (<li key={date}>
+            {date}
+          </li>)
+        })}
+      </ul>
+    </Suspense>
   );
 }
 
